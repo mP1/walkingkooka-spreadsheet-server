6b205474d10219b408a737498507d6c4a1b98b90
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2020-10-08 22:35:10 +1100

    SpreadsheetContextHateosHandlersRouterTest.testHandleMetadataCreatePost
    test for POST /api/spreadsheet with no BODY.

--- src/test/java/walkingkooka/spreadsheet/server/context/hateos/SpreadsheetContextHateosHandlersRouterTest.java
+++ src/test/java/walkingkooka/spreadsheet/server/context/hateos/SpreadsheetContextHateosHandlersRouterTest.java
@@ -111,12 +111,23 @@ public final class SpreadsheetContextHateosHandlersRouterTest extends Spreadshee
     }
 
     @Test
+    public void testHandleMetadataCreatePost() {
+        this.routeAndCheck(HttpMethod.POST,
+                URL + "/spreadsheet/",
+                HttpStatusCode.OK,
+                "{\n" +
+                        "  \"creator\": \"created@example.com\",\n" +
+                        "  \"spreadsheet-id\": \"12ef\"\n" +
+                        "}");
+    }
+
+    @Test
     public void testHandleMetadataSavePost() {
         this.routeAndCheck(HttpMethod.POST,
                 URL + "/spreadsheet/12ef",
                 HttpStatusCode.OK,
                 "{\n" +
-                        "  \"creator\": \"save@example.com\",\n" +
+                        "  \"creator\": \"saved@example.com\",\n" +
                         "  \"spreadsheet-id\": \"12ef\"\n" +
                         "}");
     }
@@ -234,13 +245,25 @@ public final class SpreadsheetContextHateosHandlersRouterTest extends Spreadshee
             public Optional<SpreadsheetMetadata> handle(final Optional<SpreadsheetId> id,
                                                         final Optional<SpreadsheetMetadata> resource,
                                                         final Map<HttpRequestAttribute<?>, Object> parameters) {
-                checkHandleParameters(id, resource, parameters);
-                return Optional.of(saveMetadataMetadata());
+                if(id.isPresent()) {
+                    checkHandleParameters(id, resource, parameters);
+                    return Optional.of(saveMetadataMetadata());
+                }
+
+                assertEquals(Optional.empty(), id, "id");
+                assertEquals(Optional.empty(), resource, "resource");
+
+                return Optional.of(createMetadataMetadata());
+            }
+
+            private SpreadsheetMetadata createMetadataMetadata() {
+                return SpreadsheetMetadata.with(Maps.of(SpreadsheetMetadataPropertyName.SPREADSHEET_ID, spreadsheetId(),
+                        SpreadsheetMetadataPropertyName.CREATOR, EmailAddress.parse("created@example.com")));
             }
 
             private SpreadsheetMetadata saveMetadataMetadata() {
                 return SpreadsheetMetadata.with(Maps.of(SpreadsheetMetadataPropertyName.SPREADSHEET_ID, spreadsheetId(),
-                        SpreadsheetMetadataPropertyName.CREATOR, EmailAddress.parse("save@example.com")));
+                        SpreadsheetMetadataPropertyName.CREATOR, EmailAddress.parse("saved@example.com")));
             }
         };
     }
