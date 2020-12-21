2a6e34b4774a05e1d66d7caf40c185e388c3fef5
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2020-12-21 18:00:56 +1100

    MultiFormatRequest json marshalling support

--- src/test/java/walkingkooka/spreadsheet/server/format/MultiFormatRequestTest.java
+++ src/test/java/walkingkooka/spreadsheet/server/format/MultiFormatRequestTest.java
@@ -20,18 +20,48 @@ package walkingkooka.spreadsheet.server.format;
 import org.junit.jupiter.api.Test;
 import walkingkooka.collect.list.Lists;
 import walkingkooka.spreadsheet.format.pattern.SpreadsheetFormatPattern;
+import walkingkooka.tree.json.JsonNode;
+import walkingkooka.tree.json.marshall.JsonNodeMarshallingTesting;
+import walkingkooka.tree.json.marshall.JsonNodeUnmarshallContext;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
-public final class MultiFormatRequestTest extends FormatterTestCase2<MultiFormatRequest> {
+public final class MultiFormatRequestTest extends FormatterTestCase2<MultiFormatRequest> implements JsonNodeMarshallingTesting<MultiFormatRequest> {
 
     @Test
     public void testWithNullRequests() {
         assertThrows(NullPointerException.class, () -> MultiFormatRequest.with(null));
     }
+
+    // Json..............................................................................................................
+
+    @Test
+    public void testJsonRoundtrip() {
+        this.marshallRoundTripTwiceAndCheck(this.createObject());
+    }
+
+    @Test
+    public void testJsonRoundtrip2() {
+        this.marshallRoundTripTwiceAndCheck(
+                MultiFormatRequest.with(
+                        Lists.of(
+                                FormatRequest.with(LocalDate.of(2000, 1, 2),
+                                        SpreadsheetFormatPattern.parseDateFormatPattern("dd/mm/yyyy")),
+                                FormatRequest.with(LocalDateTime.of(2000, 1, 2, 12, 58, 59),
+                                        SpreadsheetFormatPattern.parseDateTimeFormatPattern("dd/mm/yyyy hh:mm")),
+                                FormatRequest.with(LocalTime.of(12, 58, 59),
+                                        SpreadsheetFormatPattern.parseTimeFormatPattern("hh:mm"))
+                        )
+                )
+        );
+    }
+
+    // Object...........................................................................................................
 
     @Test
     public void testDifferentRequests() {
@@ -63,5 +93,18 @@ public final class MultiFormatRequestTest extends FormatterTestCase2<MultiFormat
     @Override
     public Class<MultiFormatRequest> type() {
         return MultiFormatRequest.class;
+    }
+
+    // Json.............................................................................................................
+
+    @Override
+    public MultiFormatRequest createJsonNodeMappingValue() {
+        return this.createObject();
+    }
+
+    @Override
+    public MultiFormatRequest unmarshall(final JsonNode node,
+                                         final JsonNodeUnmarshallContext context) {
+        return MultiFormatRequest.unmarshall(node, context);
     }
 }
