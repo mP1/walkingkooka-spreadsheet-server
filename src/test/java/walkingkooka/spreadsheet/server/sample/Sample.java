089ff33687e400626ba82625610c6da9b392e075
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2021-01-10 10:48:49 +1100

    walkingkooka-spreadsheet/issues/1202 SpreadsheetMetadata.decimalNumberContext no longer Locale defaults + 2
    - https://github.com/mP1/walkingkooka-spreadsheet/pull/1202
    - SpreadsheetMetadata.decimalNumberContext no longer Locale defaults
    - had to init some SpreadsheetMetadata.loadLocaleDefaults()
    
    - https://github.com/mP1/walkingkooka-spreadsheet/pull/1199
    - SpreadsheetParserTokenToExpressionSpreadsheetParserTokenVisitor.accept(ExpressionNumberKind) replaces ExpressionNumberContext
    
    - https://github.com/mP1/walkingkooka-spreadsheet/pull/1189
    - SpreadsheetFormula.token
    - required updates to several expected JSON response bodies

--- src/test/java/walkingkooka/spreadsheet/server/sample/Sample.java
+++ src/test/java/walkingkooka/spreadsheet/server/sample/Sample.java
@@ -157,6 +157,11 @@ public final class Sample {
         return new FakeSpreadsheetEngineContext() {
 
             @Override
+            public ExpressionNumberKind expressionNumberKind() {
+                return EXPRESSION_NUMBER_KIND;
+            }
+
+            @Override
             public SpreadsheetParserToken parseFormula(final String formula) {
                 return Cast.to(SpreadsheetParsers.expression()
                         .orFailIfCursorNotEmpty(ParserReporters.basic())
