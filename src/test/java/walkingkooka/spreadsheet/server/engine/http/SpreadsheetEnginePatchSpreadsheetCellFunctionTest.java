c58e3a0d1deee37f4c9ec75d45bdfd7dfeb9a1ae
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2023-10-26 19:50:09 +1100

    setDefaultAnchor replaces setAnchor(SpreadsheetViewportAnchor.NONE)

--- src/test/java/walkingkooka/spreadsheet/server/engine/http/SpreadsheetEnginePatchSpreadsheetCellFunctionTest.java
+++ src/test/java/walkingkooka/spreadsheet/server/engine/http/SpreadsheetEnginePatchSpreadsheetCellFunctionTest.java
@@ -38,7 +38,6 @@ import walkingkooka.spreadsheet.reference.SpreadsheetCellRange;
 import walkingkooka.spreadsheet.reference.SpreadsheetCellReference;
 import walkingkooka.spreadsheet.reference.SpreadsheetSelection;
 import walkingkooka.spreadsheet.reference.SpreadsheetViewport;
-import walkingkooka.spreadsheet.reference.SpreadsheetViewportAnchor;
 import walkingkooka.tree.json.JsonNode;
 import walkingkooka.tree.json.JsonPropertyName;
 import walkingkooka.tree.text.FontStyle;
@@ -109,7 +108,7 @@ public final class SpreadsheetEnginePatchSpreadsheetCellFunctionTest extends Spr
                                                 .setSelection(
                                                         Optional.of(
                                                                 SpreadsheetSelection.parseCell("Z99")
-                                                                        .setAnchor(SpreadsheetViewportAnchor.NONE)
+                                                                        .setDefaultAnchor()
                                                         )
                                                 )
                                 )
