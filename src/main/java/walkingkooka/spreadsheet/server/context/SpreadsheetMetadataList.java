6a400c650cba63e0fdbe00ffbe942e4819522c7e
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-04-05 13:40:15 +1100

    SpreadsheetMetadataList

---
+++ src/main/java/walkingkooka/spreadsheet/server/context/SpreadsheetMetadataList.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.server.context;
+
+import walkingkooka.spreadsheet.meta.SpreadsheetMetadata;
+import walkingkooka.tree.json.JsonNode;
+import walkingkooka.tree.json.marshall.JsonNodeContext;
+import walkingkooka.tree.json.marshall.JsonNodeMarshallContext;
+import walkingkooka.tree.json.marshall.JsonNodeUnmarshallContext;
+
+import java.util.ArrayList;
+
+/**
+ * Payload for lists of {@link SpreadsheetMetadata}.
+ */
+final class SpreadsheetMetadataList extends ArrayList<SpreadsheetMetadata> {
+
+    static {
+        JsonNodeContext.register(
+                JsonNodeContext.computeTypeName(SpreadsheetMetadataList.class),
+                SpreadsheetMetadataList::unmarshall,
+                SpreadsheetMetadataList::marshall,
+                SpreadsheetMetadataList.class
+        );
+    }
+
+    private JsonNode marshall(final JsonNodeMarshallContext context) {
+        return context.marshallCollection(this);
+    }
+
+    // @VisibleForTesting
+    static SpreadsheetMetadataList unmarshall(final JsonNode node,
+                                              final JsonNodeUnmarshallContext context) {
+        final SpreadsheetMetadataList list = empty();
+        list.addAll(
+                context.unmarshallList(
+                        node,
+                        SpreadsheetMetadata.class
+                )
+        );
+        return list;
+    }
+
+    /**
+     * Factory that creates an empty {@link SpreadsheetMetadataList}.
+     */
+    static SpreadsheetMetadataList empty() {
+        return new SpreadsheetMetadataList();
+    }
+
+    private SpreadsheetMetadataList() {
+    }
+}
