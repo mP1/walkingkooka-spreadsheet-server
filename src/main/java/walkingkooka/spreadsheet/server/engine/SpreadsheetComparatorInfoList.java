a392bf596b3b140a54dacdd901ba1fd233a44187
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-04-26 10:32:47 +1000

    SpreadsheetEngineHateosHandlerSpreadsheetComparator-2of2

---
+++ src/main/java/walkingkooka/spreadsheet/server/engine/SpreadsheetComparatorInfoList.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.server.engine;
+
+import walkingkooka.collect.list.Lists;
+import walkingkooka.spreadsheet.compare.SpreadsheetComparatorInfo;
+import walkingkooka.tree.json.JsonNode;
+import walkingkooka.tree.json.marshall.JsonNodeContext;
+import walkingkooka.tree.json.marshall.JsonNodeMarshallContext;
+import walkingkooka.tree.json.marshall.JsonNodeUnmarshallContext;
+
+import java.util.AbstractList;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * An immutable {@link List} that only exists so marshalling the list from {@link SpreadsheetEngineHateosHandlerSpreadsheetComparators}
+ * does not include type info for each element.
+ */
+public final class SpreadsheetComparatorInfoList extends AbstractList<SpreadsheetComparatorInfo> {
+
+    static SpreadsheetComparatorInfoList with(final List<SpreadsheetComparatorInfo> infos) {
+        return infos instanceof SpreadsheetComparatorInfoList ?
+                (SpreadsheetComparatorInfoList) infos :
+                new SpreadsheetComparatorInfoList(infos);
+    }
+
+    private SpreadsheetComparatorInfoList(final List<SpreadsheetComparatorInfo> infos) {
+        Objects.requireNonNull(infos, "infos");
+        if (infos.isEmpty()) {
+            throw new IllegalArgumentException("List cannot be empty");
+        }
+        this.infos = Lists.immutable(infos);
+    }
+
+    @Override
+    public SpreadsheetComparatorInfo get(final int index) {
+        return this.infos.get(index);
+    }
+
+    @Override
+    public int size() {
+        return this.infos.size();
+    }
+
+    private final List<SpreadsheetComparatorInfo> infos;
+
+    // Json.............................................................................................................
+
+    static SpreadsheetComparatorInfoList unmarshall(final JsonNode node,
+                                                    final JsonNodeUnmarshallContext context) {
+        return SpreadsheetComparatorInfoList.with(
+                context.unmarshallList(
+                        node,
+                        SpreadsheetComparatorInfo.class
+                )
+        );
+    }
+
+    private JsonNode marshall(final JsonNodeMarshallContext context) {
+        return context.marshallCollection(this);
+    }
+
+    static {
+        Lists.registerImmutableType(SpreadsheetComparatorInfoList.class);
+
+        JsonNodeContext.register(
+                JsonNodeContext.computeTypeName(SpreadsheetComparatorInfoList.class),
+                SpreadsheetComparatorInfoList::unmarshall,
+                SpreadsheetComparatorInfoList::marshall,
+                SpreadsheetComparatorInfoList.class
+        );
+    }
+}
---
+++ src/main/java/walkingkooka/spreadsheet/server/engine/SpreadsheetEngineHateosHandlerSpreadsheetComparators.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.server.engine;
+
+import walkingkooka.collect.Range;
+import walkingkooka.net.http.server.HttpRequestAttribute;
+import walkingkooka.net.http.server.hateos.HateosHandler;
+import walkingkooka.spreadsheet.compare.SpreadsheetComparatorInfo;
+import walkingkooka.spreadsheet.compare.SpreadsheetComparatorName;
+import walkingkooka.spreadsheet.engine.SpreadsheetEngine;
+import walkingkooka.spreadsheet.engine.SpreadsheetEngineContext;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * Provides a single end point to retrieve ALL the {@link SpreadsheetComparatorInfo} available to this spreadsheet.
+ * GETS for individual or a range are not supported and throw {@link UnsupportedOperationException}.
+ */
+final class SpreadsheetEngineHateosHandlerSpreadsheetComparators extends SpreadsheetEngineHateosHandler<SpreadsheetComparatorName, SpreadsheetComparatorInfo, SpreadsheetComparatorInfoList> {
+
+    static SpreadsheetEngineHateosHandlerSpreadsheetComparators with(final SpreadsheetEngine engine,
+                                                                     final SpreadsheetEngineContext context) {
+        check(
+                engine,
+                context
+        );
+        return new SpreadsheetEngineHateosHandlerSpreadsheetComparators(engine, context);
+    }
+
+    private SpreadsheetEngineHateosHandlerSpreadsheetComparators(final SpreadsheetEngine engine,
+                                                                 final SpreadsheetEngineContext context) {
+        super(engine, context);
+    }
+
+    @Override
+    public Optional<SpreadsheetComparatorInfoList> handleAll(final Optional<SpreadsheetComparatorInfoList> infos,
+                                                             final Map<HttpRequestAttribute<?>, Object> parameters) {
+        HateosHandler.checkResourceEmpty(infos);
+        HateosHandler.checkParameters(parameters);
+
+        return Optional.of(
+                SpreadsheetComparatorInfoList.with(
+                        this.context.spreadsheetComparatorInfos()
+                                .stream()
+                                .collect(Collectors.toList())
+                )
+        );
+    }
+
+    @Override
+    public Optional<SpreadsheetComparatorInfo> handleOne(final SpreadsheetComparatorName name,
+                                                         final Optional<SpreadsheetComparatorInfo> info,
+                                                         final Map<HttpRequestAttribute<?>, Object> parameters) {
+        HateosHandler.checkId(name);
+        HateosHandler.checkResource(info);
+        HateosHandler.checkParameters(parameters);
+
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Optional<SpreadsheetComparatorInfoList> handleRange(final Range<SpreadsheetComparatorName> nameRange,
+                                                               final Optional<SpreadsheetComparatorInfoList> infos,
+                                                               final Map<HttpRequestAttribute<?>, Object> parameters) {
+        HateosHandler.checkRange(nameRange);
+        HateosHandler.checkResource(infos);
+        HateosHandler.checkParameters(parameters);
+
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public String toString() {
+        return "SpreadsheetEngineContext.spreadsheetComparatorInfos";
+    }
+}
