b1ad2307f93101149694894780582dc19320064d
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-08-06 20:58:53 +1000

    SpreadsheetFormatterSelectorMenuList.prepare

--- src/test/java/walkingkooka/spreadsheet/server/formatter/SpreadsheetFormatterSelectorMenuListTest.java
+++ src/test/java/walkingkooka/spreadsheet/server/formatter/SpreadsheetFormatterSelectorMenuListTest.java
@@ -24,6 +24,7 @@ import walkingkooka.collect.list.Lists;
 import walkingkooka.reflect.ClassTesting;
 import walkingkooka.reflect.JavaVisibility;
 import walkingkooka.spreadsheet.format.SpreadsheetFormatterName;
+import walkingkooka.spreadsheet.meta.SpreadsheetMetadataTesting;
 import walkingkooka.tree.json.JsonNode;
 import walkingkooka.tree.json.marshall.JsonNodeMarshallingTesting;
 import walkingkooka.tree.json.marshall.JsonNodeUnmarshallContext;
@@ -34,7 +35,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 public class SpreadsheetFormatterSelectorMenuListTest implements ListTesting2<SpreadsheetFormatterSelectorMenuList, SpreadsheetFormatterSelectorMenu>,
         ClassTesting<SpreadsheetFormatterSelectorMenuList>,
         ImmutableListTesting<SpreadsheetFormatterSelectorMenuList, SpreadsheetFormatterSelectorMenu>,
-        JsonNodeMarshallingTesting<SpreadsheetFormatterSelectorMenuList> {
+        JsonNodeMarshallingTesting<SpreadsheetFormatterSelectorMenuList>,
+        SpreadsheetMetadataTesting {
 
     private final static SpreadsheetFormatterSelectorMenu MENU1 = SpreadsheetFormatterSelectorMenu.with(
             "Short",
@@ -171,5 +173,105 @@ public class SpreadsheetFormatterSelectorMenuListTest implements ListTesting2<Sp
     @Override
     public SpreadsheetFormatterSelectorMenuList createJsonNodeMarshallingValue() {
         return this.createList();
+    }
+
+    // prepare..........................................................................................................
+
+    @Test
+    public void testPrepareWithSpreadsheetFormatterProviderFails() {
+        assertThrows(
+                NullPointerException.class,
+                () -> SpreadsheetFormatterSelectorMenuList.prepare(
+                        null,
+                        SPREADSHEET_FORMATTER_CONTEXT
+                )
+        );
+    }
+
+    @Test
+    public void testPrepareWithSpreadsheetFormatterContextFails() {
+        assertThrows(
+                NullPointerException.class,
+                () -> SpreadsheetFormatterSelectorMenuList.prepare(
+                        SPREADSHEET_FORMATTER_PROVIDER,
+                        null
+                )
+        );
+    }
+
+    @Test
+    public void testPrepare() {
+        this.checkEquals(
+                Lists.of(
+                        SpreadsheetFormatterSelectorMenu.with(
+                                "Short",
+                                SpreadsheetFormatterName.DATE_FORMAT_PATTERN.setText("d/m/yy")
+                        ),
+                        SpreadsheetFormatterSelectorMenu.with(
+                                "Medium",
+                                SpreadsheetFormatterName.DATE_FORMAT_PATTERN.setText("d mmm yyyy")
+                        ),
+                        SpreadsheetFormatterSelectorMenu.with(
+                                "Long",
+                                SpreadsheetFormatterName.DATE_FORMAT_PATTERN.setText("d mmmm yyyy")
+                        ),
+                        SpreadsheetFormatterSelectorMenu.with(
+                                "Full",
+                                SpreadsheetFormatterName.DATE_FORMAT_PATTERN.setText("dddd, d mmmm yyyy")
+                        ),
+                        SpreadsheetFormatterSelectorMenu.with(
+                                "Short",
+                                SpreadsheetFormatterName.DATE_TIME_FORMAT_PATTERN.setText("d/m/yy, h:mm AM/PM")
+                        ),
+                        SpreadsheetFormatterSelectorMenu.with(
+                                "Medium",
+                                SpreadsheetFormatterName.DATE_TIME_FORMAT_PATTERN.setText("d mmm yyyy, h:mm:ss AM/PM")
+                        ),
+                        SpreadsheetFormatterSelectorMenu.with(
+                                "Long",
+                                SpreadsheetFormatterName.DATE_TIME_FORMAT_PATTERN.setText("d mmmm yyyy \\a\\t h:mm:ss AM/PM")
+                        ),
+                        SpreadsheetFormatterSelectorMenu.with(
+                                "Full",
+                                SpreadsheetFormatterName.DATE_TIME_FORMAT_PATTERN.setText("dddd, d mmmm yyyy \\a\\t h:mm:ss AM/PM")
+                        ),
+                        SpreadsheetFormatterSelectorMenu.with(
+                                "General",
+                                SpreadsheetFormatterName.GENERAL.setText("")
+                        ),
+                        SpreadsheetFormatterSelectorMenu.with(
+                                "Number",
+                                SpreadsheetFormatterName.NUMBER_FORMAT_PATTERN.setText("\"#,##0.###\"")
+                        ),
+                        SpreadsheetFormatterSelectorMenu.with(
+                                "Integer",
+                                SpreadsheetFormatterName.NUMBER_FORMAT_PATTERN.setText("#,##0")
+                        ),
+                        SpreadsheetFormatterSelectorMenu.with(
+                                "Percent",
+                                SpreadsheetFormatterName.NUMBER_FORMAT_PATTERN.setText("#,##0%")
+                        ),
+                        SpreadsheetFormatterSelectorMenu.with(
+                                "Currency",
+                                SpreadsheetFormatterName.NUMBER_FORMAT_PATTERN.setText("$#,##0.00")
+                        ),
+                        SpreadsheetFormatterSelectorMenu.with(
+                                "Default",
+                                SpreadsheetFormatterName.TEXT_FORMAT_PATTERN.setText("@")
+                        ),
+                        SpreadsheetFormatterSelectorMenu.with(
+                                "Short",
+                                SpreadsheetFormatterName.TIME_FORMAT_PATTERN.setText("h:mm AM/PM")
+                        ),
+                        SpreadsheetFormatterSelectorMenu.with(
+                                "Long",
+                                SpreadsheetFormatterName.TIME_FORMAT_PATTERN.setText("h:mm:ss AM/PM")
+                        )
+                ),
+                SpreadsheetFormatterSelectorMenuList.prepare(
+                        SPREADSHEET_FORMATTER_PROVIDER,
+                        SPREADSHEET_FORMATTER_CONTEXT
+                )
+        );
     }
 }
