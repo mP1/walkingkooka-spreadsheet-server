33de7ba3326de73ca004d33b9fb6d564c439a22f
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-04-24 23:00:28 +1000

    https://github.com/mP1/walkingkooka-spreadsheet/pull/6329 SpreadsheetMetadataPropertyNameExpressionFunctionAliasSetValidator
    - https://github.com/mP1/walkingkooka-spreadsheet/pull/6329
    - SpreadsheetMetadataPropertyNameExpressionFunctionAliasSetValidator

--- src/test/java/walkingkooka/spreadsheet/server/sample/Sample.java
+++ src/test/java/walkingkooka/spreadsheet/server/sample/Sample.java
@@ -185,6 +185,7 @@ public final class Sample implements walkingkooka.text.printer.TreePrintableTest
                     "  \"timeFormatter\": \"time-format-pattern hh:mm\",\n" +
                     "  \"timeParser\": \"time-parse-pattern hh:mmhh:mm:ss.000\",\n" +
                     "  \"twoDigitYear\": 31,\n" +
+                    "  \"validatorFunctions\": \"\",\n" +
                     "  \"validatorValidators\": \"\",\n" +
                     "  \"validators\": \"\",\n" +
                     "  \"valueSeparator\": \",\"\n" +
@@ -242,6 +243,7 @@ public final class Sample implements walkingkooka.text.printer.TreePrintableTest
             .set(SpreadsheetMetadataPropertyName.TIME_PARSER, SpreadsheetPattern.parseTimeParsePattern("hh:mmhh:mm:ss.000").spreadsheetParserSelector())
             .set(SpreadsheetMetadataPropertyName.TWO_DIGIT_YEAR, 31)
             .set(SpreadsheetMetadataPropertyName.VALIDATORS, ValidatorAliasSet.EMPTY)
+            .set(SpreadsheetMetadataPropertyName.VALIDATOR_FUNCTIONS, ExpressionFunctionAliasSet.EMPTY)
             .set(SpreadsheetMetadataPropertyName.VALIDATOR_VALIDATORS, ValidatorAliasSet.EMPTY)
             .set(SpreadsheetMetadataPropertyName.VALUE_SEPARATOR, ',');
 
