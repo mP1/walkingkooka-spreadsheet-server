d284992972a2f97093707597950bb60ec0c83225
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-06-03 18:56:58 +1000

    SpreadsheetServerMediaTypes.CONTENT_TYPE charset=utf8

--- src/main/java/walkingkooka/spreadsheet/server/plugin/PluginHateosHttpEntityHandlerUpload.java
+++ src/main/java/walkingkooka/spreadsheet/server/plugin/PluginHateosHttpEntityHandlerUpload.java
@@ -91,7 +91,15 @@ final class PluginHateosHttpEntityHandlerUpload implements HateosHttpEntityHandl
                 throw new IllegalArgumentException("Missing " + HttpHeaderName.CONTENT_TYPE);
             }
             // Content-type: Got text/plain, expected multipart/multipart2 or json/json2
-            throw new IllegalArgumentException(HttpHeaderName.CONTENT_TYPE + ": Got " + contentType + ", expected " + MediaType.MULTIPART_FORM_DATA + " or " + SpreadsheetServerMediaTypes.CONTENT_TYPE);
+            throw new IllegalArgumentException(
+                HttpHeaderName.CONTENT_TYPE +
+                    ": Got " +
+                    contentType.clearParameters() +
+                    ", expected " +
+                    MediaType.MULTIPART_FORM_DATA +
+                    " or " +
+                    SpreadsheetServerMediaTypes.CONTENT_TYPE.clearParameters()
+            );
         }
 
         return response;
