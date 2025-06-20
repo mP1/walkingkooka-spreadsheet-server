694fc727cb40333d0b664ee80578387bab9de9bd
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-06-20 16:02:01 +1000

    SpreadsheetHttpServerTest.testFormLoad

--- src/test/java/walkingkooka/spreadsheet/server/SpreadsheetHttpServerTest.java
+++ src/test/java/walkingkooka/spreadsheet/server/SpreadsheetHttpServerTest.java
@@ -8014,6 +8014,81 @@ public final class SpreadsheetHttpServerTest extends SpreadsheetHttpServerTestCa
         );
     }
 
+    @Test
+    public void testFormLoad() {
+        final TestHttpServer server = this.startServerAndCreateEmptySpreadsheet();
+
+        final Form<SpreadsheetExpressionReference> form = SpreadsheetForms.form(FormName.with("Form123"))
+            .setFields(
+                Lists.of(
+                    SpreadsheetForms.field(SpreadsheetSelection.A1.toExpressionReference())
+                        .setLabel("FieldLabel1")
+                )
+            );
+
+        server.handleAndCheck(
+            HttpMethod.POST,
+            "/api/spreadsheet/1/form",
+            NO_HEADERS_TRANSACTION_ID,
+            toJson(
+                SpreadsheetDelta.EMPTY.setForms(
+                    Sets.of(
+                        form
+                    )
+                )
+            ),
+            this.response(
+                HttpStatusCode.CREATED.status(),
+                this.toJson(
+                    SpreadsheetDelta.EMPTY.setForms(
+                        Sets.of(
+                            form
+                        )
+                    ).setDeletedCells(
+                        Sets.of(SpreadsheetSelection.A1)
+                    ).setColumnWidths(
+                        Maps.of(
+                            SpreadsheetSelection.parseColumn("A"),
+                            100.0
+                        )
+                    ).setRowHeights(
+                        Maps.of(
+                            SpreadsheetSelection.parseRow("1"),
+                            50.0
+                        )
+                    ).setColumnCount(
+                        OptionalInt.of(0)
+                    ).setRowCount(
+                        OptionalInt.of(0)
+                    )
+                ),
+                SpreadsheetDelta.class.getSimpleName()
+            )
+        );
+
+        server.handleAndCheck(
+            HttpMethod.GET,
+            "/api/spreadsheet/1/form/Form123",
+            NO_HEADERS_TRANSACTION_ID,
+            "",
+            this.response(
+                HttpStatusCode.OK.status(),
+                this.toJson(
+                    SpreadsheetDelta.EMPTY.setForms(
+                        Sets.of(
+                            form
+                        )
+                    ).setColumnCount(
+                        OptionalInt.of(0)
+                    ).setRowCount(
+                        OptionalInt.of(0)
+                    )
+                ),
+                SpreadsheetDelta.class.getSimpleName()
+            )
+        );
+    }
+
     // comparators......................................................................................................
 
     @Test
