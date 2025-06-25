23f16946918f2ef20cfd0485082a4a5ec5325425
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-06-25 19:01:39 +1000

    BasicLocaleHateosResourceHandlerContext

---
+++ src/main/java/walkingkooka/spreadsheet/server/locale/BasicLocaleHateosResourceHandlerContext.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2019 Miroslav Pokorny (github.com/mP1)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package walkingkooka.spreadsheet.server.locale;
+
+import walkingkooka.locale.LocaleContext;
+import walkingkooka.locale.LocaleContextDelegator;
+import walkingkooka.net.http.server.hateos.HateosResourceHandlerContext;
+import walkingkooka.net.http.server.hateos.HateosResourceHandlerContextDelegator;
+import walkingkooka.tree.json.marshall.JsonNodeUnmarshallContextPreProcessor;
+
+import java.util.Objects;
+
+final class BasicLocaleHateosResourceHandlerContext implements LocaleHateosResourceHandlerContext,
+    LocaleContextDelegator,
+    HateosResourceHandlerContextDelegator {
+
+    static BasicLocaleHateosResourceHandlerContext with(final LocaleContext localeContext,
+                                                        final HateosResourceHandlerContext hateosResourceHandlerContext) {
+        return new BasicLocaleHateosResourceHandlerContext(
+            Objects.requireNonNull(localeContext, "localeContext"),
+            Objects.requireNonNull(hateosResourceHandlerContext, "hateosResourceHandlerContext")
+        );
+    }
+
+    private BasicLocaleHateosResourceHandlerContext(final LocaleContext localeContext,
+                                                    final HateosResourceHandlerContext hateosResourceHandlerContext) {
+        super();
+
+        this.localeContext = localeContext;
+        this.hateosResourceHandlerContext = hateosResourceHandlerContext;
+    }
+
+    // LocaleContextDelegator...........................................................................................
+
+    @Override
+    public LocaleContext localeContext() {
+        return this.localeContext;
+    }
+
+    private final LocaleContext localeContext;
+
+    // HateosResourceHandlerContext.....................................................................................
+
+    @Override
+    public HateosResourceHandlerContext hateosResourceHandlerContext() {
+        return this.hateosResourceHandlerContext;
+    }
+
+    private final HateosResourceHandlerContext hateosResourceHandlerContext;
+
+    @Override
+    public HateosResourceHandlerContext setPreProcessor(final JsonNodeUnmarshallContextPreProcessor processor) {
+        final HateosResourceHandlerContext before = this.hateosResourceHandlerContext;
+        final HateosResourceHandlerContext after = before.setPreProcessor(processor);
+
+        return before.equals(after) ?
+            this :
+            with(
+                this.localeContext,
+                after
+            );
+    }
+
+    // Object...........................................................................................................
+
+    @Override
+    public String toString() {
+        return this.localeContext +
+            " " +
+            this.hateosResourceHandlerContext;
+    }
+}
