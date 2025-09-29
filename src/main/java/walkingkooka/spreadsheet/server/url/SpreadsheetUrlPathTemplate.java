061b163b092c44fb000fcba19196c2f36aaaff66
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2025-09-29 13:10:40 +1000

    SpreadsheetUrlPathTemplate raw use of parameterized class warning FIX

--- src/main/java/walkingkooka/spreadsheet/server/url/SpreadsheetUrlPathTemplate.java
+++ src/main/java/walkingkooka/spreadsheet/server/url/SpreadsheetUrlPathTemplate.java
@@ -344,7 +344,7 @@ public final class SpreadsheetUrlPathTemplate implements Template {
                         stringValue = value.toString();
                         break;
                     case "TextStylePropertyName":
-                        stringValue = ((TextStylePropertyName)value).text();
+                        stringValue = ((TextStylePropertyName<?>)value).text();
                         break;
                     default:
                         throw new IllegalArgumentException("Unknown placeholder: " + n);
