6e371e0e5d9b946f8cd59626e9b221e02caf5ed3
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-03-04 14:29:31 +1100

    https://github.com/mP1/walkingkooka-convert/pull/438 ConverterContext extends CurrencyCodeLanguageTagContext
    - https://github.com/mP1/walkingkooka-convert/pull/438
    - ConverterContext extends CurrencyCodeLanguageTagContext

--- src/test/java/walkingkooka/spreadsheet/server/formatter/BasicSpreadsheetFormatterSelectorEditContextTest.java
+++ src/test/java/walkingkooka/spreadsheet/server/formatter/BasicSpreadsheetFormatterSelectorEditContextTest.java
@@ -137,6 +137,13 @@ public final class BasicSpreadsheetFormatterSelectorEditContextTest implements S
                         ',', // valueSeparator
                         Converters.objectToString(),
                         new FakeCurrencyContext() {
+
+                            @Override
+                            public Optional<Currency> currencyForCurrencyCode(final String currencyCode) {
+                                Objects.requireNonNull(currencyCode, "currencyCode");
+                                throw new UnsupportedOperationException();
+                            }
+
                             @Override
                             public Optional<Currency> currencyForLocale(final Locale locale) {
                                 return Optional.of(
