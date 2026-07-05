f159d0ae1f26558cbb67f16e69f4a0a94c22f603
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-07-05 17:08:46 +1000

    SpreadsheetHttpServerApiSpreadsheetEngineHttpHandler inlined SpreadsheetHttpServerApiSpreadsheetEngineHttpHandlerRequest

--- src/main/java/walkingkooka/spreadsheet/server/SpreadsheetHttpServerApiSpreadsheetEngineHttpHandler.java
+++ src/main/java/walkingkooka/spreadsheet/server/SpreadsheetHttpServerApiSpreadsheetEngineHttpHandler.java
@@ -17,16 +17,21 @@
 
 package walkingkooka.spreadsheet.server;
 
+import walkingkooka.net.UrlPathName;
+import walkingkooka.net.http.HttpEntity;
+import walkingkooka.net.http.HttpStatusCode;
 import walkingkooka.net.http.server.HttpHandler;
 import walkingkooka.net.http.server.HttpHandlerContext;
 import walkingkooka.net.http.server.HttpRequest;
 import walkingkooka.net.http.server.HttpRequestAttribute;
+import walkingkooka.net.http.server.HttpRequestAttributes;
 import walkingkooka.net.http.server.HttpResponse;
 import walkingkooka.route.Router;
 import walkingkooka.spreadsheet.meta.SpreadsheetId;
 import walkingkooka.spreadsheet.server.meta.SpreadsheetMetadataHateosHandlerContexts;
 
 import java.util.Objects;
+import java.util.Optional;
 
 /**
  * A handler that routes all spreadsheet API calls.
@@ -61,11 +66,50 @@ final class SpreadsheetHttpServerApiSpreadsheetEngineHttpHandler implements Http
         Objects.requireNonNull(response, "response");
         Objects.requireNonNull(context, "context");
 
-        SpreadsheetHttpServerApiSpreadsheetEngineHttpHandlerRequest.with(
-            request,
-            response,
-            this
-        ).handle();
+        final Optional<UrlPathName> path = HttpRequestAttributes.pathComponent(SpreadsheetHttpServerApiSpreadsheetEngineHttpHandler.SPREADSHEET_ID_PATH_COMPONENT + 1)
+            .parameterValue(request);
+        if (path.isPresent()) {
+            final Optional<UrlPathName> path1 = HttpRequestAttributes.pathComponent(SpreadsheetHttpServerApiSpreadsheetEngineHttpHandler.SPREADSHEET_ID_PATH_COMPONENT)
+                .parameterValue(request);
+            if (path1.isPresent()) {
+                SpreadsheetId id;
+                try {
+                    id = SpreadsheetId.parse(path1.get().value());
+                } catch (final RuntimeException cause) {
+                    id = null;
+
+                    response.setVersion(request.protocolVersion());
+                    response.setStatus(
+                        HttpStatusCode.BAD_REQUEST.setMessage(
+                            "Invalid " + SpreadsheetId.class.getSimpleName())
+                    );
+                    response.setEntity(HttpEntity.EMPTY);
+                }
+                if (null != id) {
+                    this.router(id)
+                        .route(request.routerParameters())
+                        .orElse(SpreadsheetHttpServer::notFound)
+                        .handle(
+                            request,
+                            response,
+                            context
+                        );
+                }
+            } else {
+                response.setVersion(request.protocolVersion());
+                response.setStatus(
+                    HttpStatusCode.BAD_REQUEST.setMessage(
+                        "Missing " + SpreadsheetId.class.getSimpleName())
+                );
+                response.setEntity(HttpEntity.EMPTY);
+            }
+        } else {
+            SpreadsheetHttpServer.notFound(
+                request,
+                response,
+                context
+            );
+        }
     }
 
     // shared with SpreadsheetHttpServerApiSpreadsheetEngineHttpHandlerRequest
