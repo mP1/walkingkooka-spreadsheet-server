8877ca16e1ba82ad9b33aa6bdfcf418c4e02d41b
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-09-12 14:45:12 +1000

    SpreadsheetServerContextBasicTest.testLogXXX

--- src/test/java/walkingkooka/spreadsheet/server/SpreadsheetServerContextBasicTest.java
+++ src/test/java/walkingkooka/spreadsheet/server/SpreadsheetServerContextBasicTest.java
@@ -22,6 +22,9 @@ import walkingkooka.Cast;
 import walkingkooka.ToStringTesting;
 import walkingkooka.environment.AuditInfo;
 import walkingkooka.environment.EnvironmentValueName;
+import walkingkooka.logging.CanLog;
+import walkingkooka.logging.CanLogs;
+import walkingkooka.logging.LoggingLevel;
 import walkingkooka.net.email.EmailAddress;
 import walkingkooka.net.http.server.hateos.FakeHateosHandlerContext;
 import walkingkooka.net.http.server.hateos.HateosHandlerContext;
@@ -51,11 +54,13 @@ import walkingkooka.spreadsheet.store.repo.FakeSpreadsheetStoreRepository;
 import walkingkooka.spreadsheet.store.repo.SpreadsheetStoreRepositories;
 import walkingkooka.spreadsheet.store.repo.SpreadsheetStoreRepository;
 import walkingkooka.storage.StorageContextTesting;
+import walkingkooka.storage.StorageEnvironmentContexts;
 import walkingkooka.store.StoreWatcher;
 import walkingkooka.terminal.TerminalContext;
 import walkingkooka.terminal.TerminalId;
 import walkingkooka.terminal.server.FakeTerminalServerContext;
 import walkingkooka.terminal.server.TerminalServerContext;
+import walkingkooka.text.printer.Printers;
 import walkingkooka.text.printer.TreePrintableTesting;
 import walkingkooka.tree.json.marshall.JsonNodeMarshallContextObjectPostProcessor;
 import walkingkooka.tree.json.marshall.JsonNodeUnmarshallContextPreProcessor;
@@ -697,6 +702,72 @@ public final class SpreadsheetServerContextBasicTest implements SpreadsheetServe
         );
     }
 
+    // logXXX...........................................................................................................
+
+    private final static String MESSAGE1 = "Message111";
+    private final static String MESSAGE2 = "Message222";
+    private final static String MESSAGE3 = "Message333";
+    private final static String MESSAGE4 = "Message444";
+
+    @Test
+    public void testLogDisabled() {
+        final SpreadsheetServerContextBasic context = this.createContext(
+            CanLogs.fake()
+        );
+
+        this.isLoggingEnabledAndCheck(
+            context,
+            LoggingLevel.DEBUG,
+            false
+        );
+
+        context.debug(MESSAGE1);
+    }
+
+    @Test
+    public void testLogEnabled() {
+        final StringBuilder b = new StringBuilder();
+
+        final SpreadsheetServerContextBasic context = this.createContext(b);
+        context.setLoggingLevel(LoggingLevel.DEBUG);
+
+        this.isLoggingEnabledAndCheck(
+            context,
+            LoggingLevel.DEBUG,
+            true
+        );
+
+        context.debug(MESSAGE1);
+
+        this.checkEquals(
+            MESSAGE1 + LINE_ENDING,
+            b.toString()
+        );
+    }
+
+    @Test
+    public void testLoggingLevelChanged() {
+        final StringBuilder b = new StringBuilder();
+
+        final SpreadsheetServerContextBasic context = this.createContext(b);
+
+        context.setLoggingLevel(LoggingLevel.INFO);
+        context.debug(MESSAGE1);
+        context.info(MESSAGE2);
+
+        context.setLoggingLevel(LoggingLevel.WARN);
+        context.warn(MESSAGE3);
+
+        context.setLoggingLevel(LoggingLevel.NONE);
+        context.warn(MESSAGE4);
+
+        this.checkEquals(
+            MESSAGE2 + LINE_ENDING +
+                MESSAGE3 + LINE_ENDING,
+            b.toString()
+        );
+    }
+
     @Override
     public SpreadsheetServerContextBasic createContext() {
         return this.createContext(USER);
@@ -713,6 +784,32 @@ public final class SpreadsheetServerContextBasicTest implements SpreadsheetServe
         );
 
         return this.createContext(context);
+    }
+
+    private SpreadsheetServerContextBasic createContext(final StringBuilder b) {
+        return this.createContext(
+            CanLogs.printer(
+                Printers.stringBuilder(
+                    b,
+                    LINE_ENDING
+                )
+            )
+        );
+    }
+
+    private SpreadsheetServerContextBasic createContext(final CanLog canLog) {
+        return this.createContext(
+            SpreadsheetEnvironmentContexts.basic(
+                Cast.to(STORAGE),
+                StorageEnvironmentContexts.basic(
+                    SPREADSHEET_ENVIRONMENT_CONTEXT.cloneEnvironment()
+                        .environment()
+                        .setCanLog(canLog)
+                        .environmentContext()
+                        .cloneEnvironment()
+                )
+            )
+        );
     }
 
     private SpreadsheetServerContextBasic createContext(final SpreadsheetEnvironmentContext spreadsheetEnvironmentContext) {
