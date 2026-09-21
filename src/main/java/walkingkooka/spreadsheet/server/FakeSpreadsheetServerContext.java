a85f2bb9ad2b4f58b48da282bbfb40800bf98556
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2026-09-21 15:41:44 +1000

    https://github.com/mP1/walkingkooka-logging/pull/93 CanLog.xxx LoggingLevel loggingLevel was logging parameter name
    - https://github.com/mP1/walkingkooka-logging/pull/93
    - CanLog.xxx LoggingLevel loggingLevel was logging parameter name

--- src/main/java/walkingkooka/spreadsheet/server/FakeSpreadsheetServerContext.java
+++ src/main/java/walkingkooka/spreadsheet/server/FakeSpreadsheetServerContext.java
@@ -278,13 +278,13 @@ public class FakeSpreadsheetServerContext extends FakeSpreadsheetProvider implem
     }
 
     @Override
-    public void log(final LoggingLevel level,
+    public void log(final LoggingLevel loggingLevel,
                     final String message) {
         throw new UnsupportedOperationException();
     }
 
     @Override
-    public void log(final LoggingLevel level,
+    public void log(final LoggingLevel loggingLevel,
                     final String message,
                     final Throwable throwable) {
         throw new UnsupportedOperationException();
@@ -316,7 +316,7 @@ public class FakeSpreadsheetServerContext extends FakeSpreadsheetProvider implem
     }
 
     @Override
-    public boolean isLoggingEnabled(final LoggingLevel level) {
+    public boolean isLoggingEnabled(final LoggingLevel loggingLevel) {
         throw new UnsupportedOperationException();
     }
 
